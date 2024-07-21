c1f6257015d84bea69b22c8136a7700a59b0032e
Franklin Kong <franklinkong981@gmail.com>
2024-07-21 16:12:17 -0700

    Refactored code so functions are smaller and testable

--- step3.js
+++ step3.js
@@ -2,48 +2,40 @@ const fs = require('fs');
 const axios = require('axios');
 const argv = process.argv;
 
-function cat(path) {
+function cat(path, writePath = null) {
     fs.readFile(path, 'utf8', (error, data) => {
         if (error) { 
             console.log("ERROR: ", error);
             process.kill(1); 
         }
-        console.log("FILE CONTENTS: ", data);
+        if (writePath) {
+            catWrite(writePath, data);
+        }
+        else {
+            console.log("FILE CONTENTS: ", data);
+        }
     });    
 }
 
-function catWrite(path, writePath) {
-    fs.readFile(path, 'utf8', (error, data) => {
-        if (error) { 
-            console.log("ERROR: ", error);
-            process.kill(1); 
+function catWrite(writePath, data) {
+    fs.writeFile(writePath, data, 'utf8', err => {
+        if (err) {
+            console.log("ERROR: ", err);
+            process.kill(1);
         }
-        fs.writeFile(writePath, data, 'utf8', err => {
-            if (err) {
-                console.log("ERROR: ", err);
-                process.kill(1);
-            }
-            console.log(`No output, but ${writePath} contains contents of ${path}`);
-        });
-    });  
+        console.log(`No output, but ${writePath} contains contents of ${writePath}`);
+    });
 }
 
-async function webCat(url) {
+async function webCat(url, writePath = null) {
     try {
         const {data} = await axios.get(url);
-        console.log("URL CONTENTS: ", data);
-    }
-    catch(error) {
-        console.log(error);
-    }
-}
-
-async function webCatWrite(url, writePath) {
-    try {
-        const {data} = await axios.get(url);
-        fs.writeFile(writePath, data, 'utf8', err => {
-            console.log(`No output, but ${writePath} contains HTML of ${url}`);
-        });
+        if (writePath) {
+            webCatWrite(writePath, data, url);
+        }
+        else {
+            console.log("URL CONTENTS: ", data);
+        }
     }
     catch(error) {
         console.log(error);
@@ -51,17 +43,27 @@ async function webCatWrite(url, writePath) {
     }
 }
 
-if (argv[2] == '--out') {
-    if (argv[4].includes('http://') || argv[4].includes('https://')) {
-        webCatWrite(argv[4], argv[3]);
+function webCatWrite(writePath, data, url) {
+    fs.writeFile(writePath, data, 'utf8', err => {
+        console.log(`No output, but ${writePath} contains HTML of ${url}`);
+    });
+}
+
+function parseArgs() {
+    if (argv[2] == '--out') {
+        if (argv[4].includes('http://') || argv[4].includes('https://')) {
+            webCat(argv[4], argv[3]);
+        }
+        else {
+            cat(argv[4], argv[3]);
+        }
+    }
+    else if (argv[2].includes('http://') || argv[2].includes('https://')) {
+        webCat(argv[2]);
     }
     else {
-        catWrite(argv[4], argv[3]);
+        cat(argv[2]);
     }
 }
-else if (argv[2].includes('http://') || argv[2].includes('https://')) {
-    webCat(argv[2]);
-}
-else {
-    cat(argv[2]);
-}
+
+parseArgs();
